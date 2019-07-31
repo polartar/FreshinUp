392c15104fddcd37aeb2673b63d95951d2f11a07
Matt Palermo <palermomatt@gmail.com>
2019-07-31 11:03:27 -0400

    Update user levels and types

---
+++ database/seeds/UserLevelTypeSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace FoodFleet\Seeds;
+
+use FreshinUp\FreshBusForms\Models\User\UserLevel;
+use FreshinUp\FreshBusForms\Models\User\UserType;
+use Illuminate\Database\Seeder;
+
+class UserLevelTypeSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $levels = [
+            1 => 'Food Fleet Admin',
+            2 => 'Food Fleet Director',
+            3 => 'Food Fleet Lead',
+            4 => 'Food Fleet Member',
+        ];
+
+        $types = [
+            1 => 'FF Staff - Admins',
+            2 => 'FF Staff',
+            3 => 'Customer',
+            4 => 'Customer - Employee',
+            5 => 'Supplier',
+            6 => 'Supplier - Employee',
+        ];
+
+        foreach($levels as $displyId => $name) {
+            $forPlatform = $displyId < 5 ? 1 : 0;
+            $forCompany = $displyId > 3 ? 1 : 0;
+            UserLevel::updateOrCreate(
+                ['display_id' => $displyId],
+                [
+                    'name' => $name,
+                    'enabled' => 1,
+                    'forPlatform' => $forPlatform,
+                    'forCompany' => $forCompany,
+                ]
+            );
+        }
+
+        foreach($types as $displyId => $name) {
+            UserType::updateOrCreate(
+                ['display_id' => $displyId],
+                ['name' => $name]
+            );
+        }
+    }
+}
