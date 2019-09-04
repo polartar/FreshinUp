add854c774e4d4dd59eeca0abde548cb7ae14df4
Giuseppe <giuseppe@nausdream.com>
2019-09-04 18:56:37 -0500

    Cleanup classes

--- tests/Feature/Unit/Models/ItemTest.php
+++ tests/Feature/Unit/Models/ItemTest.php
@@ -2,17 +2,10 @@
 
 namespace Tests\Feature\Unit\Models\Item;
 
-use App\Models\Foodfleet\Event;
-use App\Models\Foodfleet\Location;
 use App\Models\Foodfleet\Square\Category;
-use App\Models\Foodfleet\Square\Customer;
-use App\Models\Foodfleet\Square\Device;
 use App\Models\Foodfleet\Square\Item;
 use App\Models\Foodfleet\Square\Payment;
-use App\Models\Foodfleet\Square\PaymentType;
-use App\User;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Laravel\Passport\Passport;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
