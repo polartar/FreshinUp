09b92474fad9e117730a9ae6aa802dd990a9af83
Giuseppe <giuseppe@nausdream.com>
2019-09-24 20:42:01 -0500

    Fix header table

--- resources/js/components/financials/TransactionList.vue
+++ resources/js/components/financials/TransactionList.vue
@@ -201,17 +201,3 @@ export default {
   }
 }
 </script>
-<style lang="styl" scoped>
-  /deep/ .v-datatable thead {
-    background: var(--v-secondary-base) !important
-  }
-  /deep/ table.v-table thead tr {
-    height: 30px;
-  }
-  /deep/ table.v-table tbody tr {
-    height: 78px;
-  }
-  /deep/ .v-datatable th {
-    color: white !important
-  }
-</style>
