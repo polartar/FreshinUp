092442305163618728714e3282db835eec4765e7
Giuseppe <giuseppe@nausdream.com>
2019-10-05 21:11:35 -0500

    Made autocomplete on supplier dynamic

--- resources/js/components/financials/BasicFilter.vue
+++ resources/js/components/financials/BasicFilter.vue
@@ -102,7 +102,7 @@
       </v-layout>
       <simple
         :key="storeKey"
-        url="foodfleet/stores"
+        :url="storeUrl"
         term-param="filter[name]"
         results-id-key="uuid"
         :value="filters.store_uuid"
@@ -196,6 +196,13 @@ export default {
         return result + param
       }, '')
       return '/admin/financials/transactions?' + encodeURI(preparedParams.slice(1))
+    },
+    storeUrl () {
+      let url = 'foodfleet/stores'
+      if (this.filters.supplier_uuid) {
+        url += '?filter[supplier_uuid]=' + this.filters.supplier_uuid
+      }
+      return url
     }
   },
   methods: {
