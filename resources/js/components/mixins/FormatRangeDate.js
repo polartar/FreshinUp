e207ac97746c717da3f306894fcf52e402a84db0
codevon <liang@freshinup.com>
2019-10-15 11:50:50 +0800

    updated FormatRangeDate on event

--- resources/js/components/mixins/FormatRangeDate.js
+++ resources/js/components/mixins/FormatRangeDate.js
@@ -1,12 +1,12 @@
-import moment from 'moment'
+import FormatDate from 'fresh-bus/components/mixins/FormatDate'
 export default {
   methods: {
     formatRangeDate (startDate, endDate) {
       const formatArr = [ 'MMM', 'DD', '/ YYYY', 'h:mm A' ]
       if (!startDate || !endDate) return null
       return formatArr.map(format => {
-        const startFormat = moment(startDate).format(format)
-        const endFormat = moment(endDate).format(format)
+        const startFormat = FormatDate.methods.formatDate(startDate, format)
+        const endFormat = FormatDate.methods.formatDate(endDate, format)
         return startFormat === endFormat ? startFormat : startFormat + ' - ' + endFormat
       }).join(' ')
     }
