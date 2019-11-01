0106483a6de963bf7ac611e14f053d245f90b067
Nils Alexandre <nilsalexandre@gerenciagram.com.br>
2019-10-31 21:01:33 -0300

    Created fleet members page tests

--- tests/Javascript/__data__/stores.js
+++ tests/Javascript/__data__/stores.js
@@ -280,3 +280,603 @@ export const FIXTURE_STORES = [
     'square_id': '12010'
   }
 ]
+
+export const FIXTURE_STORES_RESPONSE = {
+  'data': [
+    {
+      'uuid': '2e76e6cd-a60e-4428-91df-b198d0ad854b',
+      'name': 'voluptate',
+      'status': 5,
+      'addresses': [
+        {
+          'id': 1,
+          'street': '9744 Kemmer Pine Suite 678',
+          'street_2': '91070 Sporer Orchard Apt. 308',
+          'city': 'Bayermouth',
+          'post_code': '24893-8844',
+          'country': {
+            'id': 84,
+            'name': 'GR'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': '65b8b512-79b2-460e-b141-67cbc7271bca',
+          'name': 'est'
+        }
+      ],
+      'square_id': '27734'
+    },
+    {
+      'uuid': '95433f2f-e814-4d4c-bec1-05669d113e16',
+      'name': 'eius',
+      'status': 6,
+      'addresses': [
+        {
+          'id': 2,
+          'street': '2857 Richie Lodge Suite 937',
+          'street_2': '3099 Alize Gardens',
+          'city': 'Cydneyview',
+          'post_code': '63364-0708',
+          'country': {
+            'id': 189,
+            'name': 'VC'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': 'eb73fd2c-51a3-473c-b4cb-f83f7fc73c8c',
+          'name': 'reprehenderit'
+        }
+      ],
+      'square_id': '44773'
+    },
+    {
+      'uuid': '9afecc3e-2bfd-42c6-9e6c-946b71b479f7',
+      'name': 'autem',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 3,
+          'street': '278 Annalise Roads Suite 031',
+          'street_2': '264 Mittie Mountains Apt. 565',
+          'city': 'East Marcelinastad',
+          'post_code': '67501-9315',
+          'country': {
+            'id': 5,
+            'name': 'AS'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': '6fd6b25d-6d22-4dea-a8cc-6beb2b7b3008',
+          'name': 'quia'
+        }
+      ],
+      'square_id': '6592'
+    },
+    {
+      'uuid': 'd5daf004-0335-443e-8150-4f2d84f2859f',
+      'name': 'et',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 4,
+          'street': '52044 Gorczany Skyway Apt. 095',
+          'street_2': '9180 Brando Ports',
+          'city': 'Bahringerland',
+          'post_code': '21559',
+          'country': {
+            'id': 186,
+            'name': 'LC'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': '9aa67767-2cd9-474a-a02a-2bff7fc4cf6e',
+          'name': 'et'
+        }
+      ],
+      'square_id': '91538'
+    },
+    {
+      'uuid': '2e03bf8d-d611-4ae8-b3d5-b0afe7fa2297',
+      'name': 'et',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 5,
+          'street': '842 Lehner Parkways Apt. 581',
+          'street_2': '2772 Cummings Walk Suite 104',
+          'city': 'Olliehaven',
+          'post_code': '73628-3536',
+          'country': {
+            'id': 135,
+            'name': 'MT'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': '65b8b512-79b2-460e-b141-67cbc7271bca',
+          'name': 'est'
+        }
+      ],
+      'square_id': '93915'
+    },
+    {
+      'uuid': '306f6f9b-0ab4-461d-9b6f-2c1435b566d9',
+      'name': 'natus',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 6,
+          'street': '228 Rod Port',
+          'street_2': '51662 Fay Track',
+          'city': 'South Jeffton',
+          'post_code': '26036',
+          'country': {
+            'id': 179,
+            'name': 'RE'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': 'eb73fd2c-51a3-473c-b4cb-f83f7fc73c8c',
+          'name': 'reprehenderit'
+        }
+      ],
+      'square_id': '9983'
+    },
+    {
+      'uuid': '246cf4dc-960a-4354-b96a-0fe305b39857',
+      'name': 'sunt',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 7,
+          'street': '53180 Makenna Canyon',
+          'street_2': '30447 Price Inlet',
+          'city': 'Josiannestad',
+          'post_code': '18354',
+          'country': {
+            'id': 52,
+            'name': 'CK'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': 'f96cb458-1544-43ef-8524-ca9177e69ecb',
+          'name': 'ratione'
+        }
+      ],
+      'square_id': '5920'
+    },
+    {
+      'uuid': '5c5694b6-64e5-4445-b441-18e6663b25a7',
+      'name': 'voluptatem',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 8,
+          'street': '503 Cronin Mountain Suite 457',
+          'street_2': '99264 Ward Roads Apt. 446',
+          'city': 'Manuelaside',
+          'post_code': '64047',
+          'country': {
+            'id': 158,
+            'name': 'NI'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': '6fd6b25d-6d22-4dea-a8cc-6beb2b7b3008',
+          'name': 'quia'
+        }
+      ],
+      'square_id': '20208'
+    },
+    {
+      'uuid': 'a3f78a4e-129c-4f4f-8e9c-3df3c2e72981',
+      'name': 'ipsum',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 9,
+          'street': '8809 Little Forest',
+          'street_2': '66732 Dickens Stream',
+          'city': 'Swaniawskihaven',
+          'post_code': '07221',
+          'country': {
+            'id': 198,
+            'name': 'SG'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': '9aa67767-2cd9-474a-a02a-2bff7fc4cf6e',
+          'name': 'et'
+        }
+      ],
+      'square_id': '59345'
+    },
+    {
+      'uuid': 'd9a3cfde-a9c4-490a-973b-3c478fd8327b',
+      'name': 'suscipit',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 10,
+          'street': '87556 Watsica Roads',
+          'street_2': '910 Chaz Run Apt. 909',
+          'city': 'Lake Jailynfort',
+          'post_code': '58519',
+          'country': {
+            'id': 229,
+            'name': 'UA'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': '65b8b512-79b2-460e-b141-67cbc7271bca',
+          'name': 'est'
+        }
+      ],
+      'square_id': '12010'
+    }
+  ],
+  'links': {
+    'first': 'http://foodfleet.fresh/api/foodfleet/stores?page%5Bsize%5D=10&include=tags%2Caddresses&page%5Bnumber%5D=1',
+    'last': 'http://foodfleet.fresh/api/foodfleet/stores?page%5Bsize%5D=10&include=tags%2Caddresses&page%5Bnumber%5D=6',
+    'prev': null,
+    'next': 'http://foodfleet.fresh/api/foodfleet/stores?page%5Bsize%5D=10&include=tags%2Caddresses&page%5Bnumber%5D=2'
+  },
+  'meta': {
+    'current_page': 1,
+    'from': 1,
+    'last_page': 6,
+    'path': 'http://foodfleet.fresh/api/foodfleet/stores',
+    'per_page': 10,
+    'to': 10,
+    'total': 51
+  }
+}
+
+export const FIXTURE_STORES_SORTED_BY_FIRSTNAME = {
+  'data': [
+    {
+      'uuid': 'b78e47f7-9da3-425e-8543-9137e0622b35',
+      'name': 'a',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 26,
+          'street': '762 Bode Ports Suite 667',
+          'street_2': '389 Hodkiewicz Mall',
+          'city': 'Johnsonview',
+          'post_code': '41587',
+          'country': {
+            'id': 183,
+            'name': 'BL'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': 'eb73fd2c-51a3-473c-b4cb-f83f7fc73c8c',
+          'name': 'reprehenderit'
+        }
+      ],
+      'square_id': '21037'
+    },
+    {
+      'uuid': '8e329426-cb79-495f-ab54-6816c727e2d3',
+      'name': 'a',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 33,
+          'street': '88449 Gottlieb Bypass',
+          'street_2': '6153 Gleichner Cliff',
+          'city': 'Cathychester',
+          'post_code': '43580',
+          'country': {
+            'id': 122,
+            'name': 'LS'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': '6fd6b25d-6d22-4dea-a8cc-6beb2b7b3008',
+          'name': 'quia'
+        }
+      ],
+      'square_id': '63038'
+    },
+    {
+      'uuid': 'dc30a15b-2b56-440f-a5ed-1fbe658f321d',
+      'name': 'adipisci',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 28,
+          'street': '548 Louisa Turnpike Apt. 800',
+          'street_2': '8447 Deangelo Plain Apt. 529',
+          'city': 'Pacochabury',
+          'post_code': '81150',
+          'country': {
+            'id': 231,
+            'name': 'GB'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': '6fd6b25d-6d22-4dea-a8cc-6beb2b7b3008',
+          'name': 'quia'
+        }
+      ],
+      'square_id': '68406'
+    },
+    {
+      'uuid': '58ec7abe-b017-4db2-afcd-37b480a9df6a',
+      'name': 'alias',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 32,
+          'street': '54430 Darien Roads Apt. 263',
+          'street_2': '3216 Noe Dale Apt. 235',
+          'city': 'New Dorianburgh',
+          'post_code': '98912',
+          'country': {
+            'id': 35,
+            'name': 'BF'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': 'f96cb458-1544-43ef-8524-ca9177e69ecb',
+          'name': 'ratione'
+        }
+      ],
+      'square_id': '47660'
+    },
+    {
+      'uuid': '9a5b444a-150d-4073-89cf-3bc334c3ce2c',
+      'name': 'amet',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 31,
+          'street': '59558 Rath Ridges',
+          'street_2': '4724 Hayes Knoll Suite 810',
+          'city': 'Mayertshire',
+          'post_code': '18077-7684',
+          'country': {
+            'id': 203,
+            'name': 'ZA'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': 'eb73fd2c-51a3-473c-b4cb-f83f7fc73c8c',
+          'name': 'reprehenderit'
+        }
+      ],
+      'square_id': '66885'
+    },
+    {
+      'uuid': '9afecc3e-2bfd-42c6-9e6c-946b71b479f7',
+      'name': 'autem',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 3,
+          'street': '278 Annalise Roads Suite 031',
+          'street_2': '264 Mittie Mountains Apt. 565',
+          'city': 'East Marcelinastad',
+          'post_code': '67501-9315',
+          'country': {
+            'id': 5,
+            'name': 'AS'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': '6fd6b25d-6d22-4dea-a8cc-6beb2b7b3008',
+          'name': 'quia'
+        }
+      ],
+      'square_id': '6592'
+    },
+    {
+      'uuid': '1f5a858a-1179-455f-b376-d53ef2512d07',
+      'name': 'cupiditate',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 13,
+          'street': '199 Kunde Plaza',
+          'street_2': '860 Hegmann Forges Apt. 309',
+          'city': 'Schoenfort',
+          'post_code': '12619-1555',
+          'country': {
+            'id': 3,
+            'name': 'AL'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': '6fd6b25d-6d22-4dea-a8cc-6beb2b7b3008',
+          'name': 'quia'
+        }
+      ],
+      'square_id': '80077'
+    },
+    {
+      'uuid': '88add25f-f482-4d9e-bcc2-54ef916fe686',
+      'name': 'dolorem',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 20,
+          'street': '21959 Gregg Trace Apt. 894',
+          'street_2': '1118 Bartoletti Trace Apt. 272',
+          'city': 'Abbieshire',
+          'post_code': '76921',
+          'country': {
+            'id': 132,
+            'name': 'MY'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': '65b8b512-79b2-460e-b141-67cbc7271bca',
+          'name': 'est'
+        }
+      ],
+      'square_id': '46004'
+    },
+    {
+      'uuid': 'f055a558-99f1-487e-911f-21a97fc7cec6',
+      'name': 'dolores',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 47,
+          'street': '284 Octavia Drive Apt. 474',
+          'street_2': '63486 Mariah Ridges',
+          'city': 'West Samara',
+          'post_code': '17599-9367',
+          'country': {
+            'id': 5,
+            'name': 'AS'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': 'f96cb458-1544-43ef-8524-ca9177e69ecb',
+          'name': 'ratione'
+        }
+      ],
+      'square_id': '78064'
+    },
+    {
+      'uuid': '3163fc37-8de3-40ae-b0b4-a3040e68bab7',
+      'name': 'ea',
+      'status': 1,
+      'addresses': [
+        {
+          'id': 48,
+          'street': '5916 Ibrahim Square',
+          'street_2': '254 Roger Crossroad Suite 225',
+          'city': 'Port Priceberg',
+          'post_code': '64208-0616',
+          'country': {
+            'id': 127,
+            'name': 'LU'
+          },
+          'is_primary': 0,
+          'is_billing': 0,
+          'is_shipping': 0
+        }
+      ],
+      'tags': [
+        {
+          'uuid': '6fd6b25d-6d22-4dea-a8cc-6beb2b7b3008',
+          'name': 'quia'
+        }
+      ],
+      'square_id': '63692'
+    }
+  ],
+  'links': {
+    'first': 'http://foodfleet.fresh/api/foodfleet/stores?page%5Bsize%5D=10&sort=name&include=tags%2Caddresses&page%5Bnumber%5D=1',
+    'last': 'http://foodfleet.fresh/api/foodfleet/stores?page%5Bsize%5D=10&sort=name&include=tags%2Caddresses&page%5Bnumber%5D=6',
+    'prev': null,
+    'next': 'http://foodfleet.fresh/api/foodfleet/stores?page%5Bsize%5D=10&sort=name&include=tags%2Caddresses&page%5Bnumber%5D=2'
+  },
+  'meta': {
+    'current_page': 1,
+    'from': 1,
+    'last_page': 6,
+    'path': 'http://foodfleet.fresh/api/foodfleet/stores',
+    'per_page': 10,
+    'to': 10,
+    'total': 51
+  }
+}
---
+++ tests/Javascript/pages/admin/fleet-members/index.test.js
@@ -0,0 +1,136 @@
+import { shallowMount } from '@vue/test-utils'
+import { createStore } from 'fresh-bus/store'
+import { createLocalVue } from 'fresh-bus/tests/utils'
+import { FIXTURE_STORES_RESPONSE, FIXTURE_STORES_SORTED_BY_FIRSTNAME } from 'tests/__data__/stores'
+import Component from '~/pages/admin/fleet-members/index.vue'
+import stores from '~/store/modules/stores'
+import storeStatuses from '~/store/modules/storeStatuses'
+
+describe('Admin Docs Page', () => {
+  let localVue, mock, store, actions
+  describe('Mount', () => {
+    beforeEach(() => {
+      const vue = createLocalVue({ validation: true })
+      localVue = vue.localVue
+      mock = vue.mock
+    })
+
+    afterEach(() => {
+      mock.restore()
+    })
+
+    test('snapshot', async () => {
+      const vue = createLocalVue({ validation: true })
+      localVue = vue.localVue
+      mock = vue.mock
+        .onGet('api/foodfleet/stores', { params: { 'page[size]': 10, 'page[number]': 1, sort: 'first_name' } })
+        .reply(200, { data: FIXTURE_STORES_SORTED_BY_FIRSTNAME })
+        .onGet('api/foodfleet/stores').reply(200, FIXTURE_STORES_RESPONSE)
+        .onAny()
+        .reply(config => {
+          console.warn('No mock match for ' + config.url, config)
+          return [404, {}]
+        })
+      const store = createStore({}, {
+        modules: {
+          stores: stores({}),
+          storeStatuses: storeStatuses({})
+        }
+      })
+      const wrapper = shallowMount(Component, {
+        localVue: localVue,
+        store
+      })
+      // Action: change State Machine's state
+      await wrapper.vm.$store.dispatch('page/setLoading', false)
+      await wrapper.vm.$nextTick()
+      await wrapper.vm.$store.dispatch('stores/getItems')
+      await wrapper.vm.$nextTick()
+      expect(wrapper.element).toMatchSnapshot()
+    })
+  })
+
+  describe('Methods', () => {
+    beforeEach(() => {
+      const vue = createLocalVue({ validation: true })
+      const docModule = stores({})
+      localVue = vue.localVue
+      actions = {
+        patchItem: jest.fn()
+      }
+      store = createStore({
+        docs: {
+          items: FIXTURE_STORES_RESPONSE
+        }
+      }, {
+        modules: {
+          stores: { ...docModule, actions: { ...docModule.actions, ...actions } }
+        }
+      })
+    })
+
+    afterEach(() => {
+      mock.restore()
+    })
+
+    test('changeStatus function change doc status', async () => {
+      const wrapper = shallowMount(Component, {
+        localVue: localVue,
+        store
+      })
+
+      wrapper.vm.changeStatus(2, { uuid: 'mock uuid' })
+      const data = { data: { status: 2 }, params: { id: 'mock uuid' } }
+      expect(actions.patchItem).toHaveBeenCalled()
+      expect(actions.patchItem.mock.calls).toHaveLength(1)
+      expect(actions.patchItem.mock.calls[0][1]).toEqual(data)
+    })
+
+    test('changeStatusMultiple function change doc status for each', async () => {
+      const wrapper = shallowMount(Component, {
+        localVue: localVue,
+        store
+      })
+
+      wrapper.vm.changeStatusMultiple(3, [{ uuid: 'mock uuid 1' }, { uuid: 'mock uuid 2' }])
+
+      expect(actions.patchItem).toHaveBeenCalled()
+      expect(actions.patchItem.mock.calls).toHaveLength(2)
+
+      const firstData = { data: { status: 3 }, params: { id: 'mock uuid 1' } }
+      const secondData = { data: { status: 3 }, params: { id: 'mock uuid 2' } }
+
+      expect(actions.patchItem.mock.calls[0][1]).toEqual(firstData)
+      expect(actions.patchItem.mock.calls[1][1]).toEqual(secondData)
+    })
+
+    test('onPaginate function change paginate', () => {
+      const wrapper = shallowMount(Component, {
+        localVue: localVue,
+        store
+      })
+
+      wrapper.vm.onPaginate({
+        rowsPerPage: 2,
+        totalItems: 5,
+        page: 2
+      })
+      expect(wrapper.vm.pagination.rowsPerPage).toBe(2)
+    })
+
+    test('deleteStore function change deleteTemp', () => {
+      const wrapper = shallowMount(Component, {
+        localVue: localVue,
+        store
+      })
+
+      wrapper.vm.deleteStore({ id: 1 })
+      expect(wrapper.vm.deleteTemp[0].id).toBe(1)
+      expect(wrapper.vm.deleteDialog).toBeTruthy()
+
+      wrapper.vm.deleteStore([{ id: 1 }])
+      expect(wrapper.vm.deleteTemp[0].id).toBe(1)
+      expect(wrapper.vm.deleteDialog).toBeTruthy()
+    })
+  })
+})
