89a8f9def2c4e076340fb9856fa1ce682b914b31
codevon <hngliang@gmail.com>
2019-11-07 16:59:13 +0800

    events/StoreFilter add test(not pass)

---
+++ tests/Javascript/components/events/StoreFilter.test.js
@@ -0,0 +1,49 @@
+import { createLocalVue, shallowMount, mount } from '@vue/test-utils'
+import Component from '~/components/events/StoreFilter.vue'
+import { FIXTURE_EVENTS_FLEET_MEMBER } from 'tests/__data__/eventsStore'
+
+describe('event Fleet Member Sorter component', () => {
+  let localVue
+  describe('Snapshots', () => {
+    test('types set', () => {
+      localVue = createLocalVue()
+      const wrapper = mount(Component, {
+        localVue: localVue,
+        propsData: {
+          filters: { filters: { tags: FIXTURE_EVENTS_FLEET_MEMBER['types'] } }
+        }
+      })
+
+      expect(wrapper.element).toMatchSnapshot()
+    })
+
+    test('types empty', () => {
+      localVue = createLocalVue()
+      const wrapper = mount(Component, {
+        localVue: localVue,
+        propsData: {
+          filters: { filters: { tags: [] } }
+        }
+      })
+
+      expect(wrapper.element).toMatchSnapshot()
+    })
+  })
+
+  describe('Action', () => {
+    test('clearFilters function clear filters', () => {
+      const localVue = createLocalVue()
+
+      const wrapper = mount(Component, {
+        localVue: localVue,
+        propsData: {
+          filters: { filters: { tags: FIXTURE_EVENTS_FLEET_MEMBER['types'] } }
+        }
+      })
+      wrapper.vm.clearFilters({})
+      expect(wrapper.vm.filters.location).toBeNull()
+      expect(wrapper.vm.filters.type).toBeNull()
+      expect(wrapper.vm.filters.tag).toBeNull()
+    })
+  })
+})
