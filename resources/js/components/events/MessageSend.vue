a844c59b425338b94dfcc01c9c371cef4b89602b
codevon <liang@freshinup.com>
2019-11-21 10:40:49 +0800

    update event fleet member MessageSend component

---
+++ resources/js/components/events/MessageSend.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-layout
+    row
+    wrap
+    pa-3
+    justify-space-between
+  >
+    <v-flex
+      md7
+      sm12
+    >
+      <v-textarea
+        v-model="message"
+        v-validate="'required'"
+        :error-messages="errors.collect('message')"
+        data-vv-name="message"
+        placeholder="Type Message"
+        outline
+        single-line
+      />
+      <v-btn
+        class="mr-0 right primary"
+        :disabled="!isValid"
+        @click="whenValid(send)"
+      >
+        Send
+      </v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+import Validate from 'fresh-bus/components/mixins/Validate'
+export default {
+  mixins: [Validate],
+  data () {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    send () {
+      this.$emit('send-message', this.message)
+    }
+  }
+}
+</script>
