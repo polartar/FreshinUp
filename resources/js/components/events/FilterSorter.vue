9b817a9044bdfdb37076a259e0ef375fc04bbda7
codevon <liang@freshinup.com>
2019-11-25 20:51:57 +0100

    fix lint

--- resources/js/components/events/FilterSorter.vue
+++ resources/js/components/events/FilterSorter.vue
@@ -11,8 +11,10 @@
     @run="run"
   >
     <template v-slot:title>
-      <h3 class="headline">Filter events</h3>
-      <hr/>
+      <h3 class="headline">
+        Filter events
+      </h3>
+      <hr>
     </template>
     <template v-slot:filters="slotProps">
       <v-card-text class="px-0 pb-0">
@@ -144,7 +146,6 @@
 import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css'
 import ClearButton from '~/components/ClearButton'
 import FilterLabel from '~/components/FilterLabel'
-import VueCtkDateTimePicker from 'vue-ctk-date-time-picker'
 import SearchFilterSorter from 'fresh-bus/components/search/filter-sorter'
 import MultiSelect from 'fresh-bus/components/ui/FMultiSelect'
 import MultiSimple from 'fresh-bus/components/ui/FMultiSimple'
@@ -154,7 +155,6 @@ export default {
     FilterLabel,
     ClearButton,
     MultiSelect,
-    VueCtkDateTimePicker,
     SearchFilterSorter
   },
   props: {
