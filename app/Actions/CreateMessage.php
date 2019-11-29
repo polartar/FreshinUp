ed702e9fd67d35aa546f4bfd1d85881de8582e90
codevon <hngliang@gmail.com>
2019-11-29 15:49:58 +0800

    send message email

--- app/Actions/CreateMessage.php
+++ app/Actions/CreateMessage.php
@@ -2,10 +2,14 @@
 
 namespace App\Actions;
 
+use App\User;
+use App\Models\Foodfleet\Event;
 use App\Helpers\SquareHelper;
 use FreshinUp\FreshBusForms\Actions\Action;
 use App\Models\Foodfleet\Message;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\MessageShipped;
 
 class CreateMessage implements Action
 {
@@ -15,7 +19,24 @@ class CreateMessage implements Action
         $createData = $collection->all();
 
         $message = Message::create($createData);
-
+        
+        // need to mailtrap account config info
+        // $this->sendMessage($message);
+        
         return $message->refresh();
+    }
+
+    private function sendMessage(Message $message)
+    {
+        $manager = Event::with('manager')->where('uuid', $message->event_uuid)->first()->manager;
+        $user = User::where('uuid', $message->created_by_uuid)->first();
+
+        if (!empty($user->email) && !empty($manager->email)) {
+
+            $name = $manager->first_name . ' ' . $manager->last_name;
+
+            Mail::to($manager->email, $name)
+                    ->send(new MessageShipped($user, $message));
+        }
     }
 }
