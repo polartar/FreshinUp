c4b2673f25d7986fb2d7625b6d2915456976ecd9
codevon <hngliang@gmail.com>
2019-12-03 13:20:58 +0800

    add event settings section

---
+++ resources/js/components/events/EventSettingsModal.stories.js
@@ -0,0 +1,28 @@
+import { storiesOf } from '@storybook/vue'
+import { action } from '@storybook/addon-actions'
+
+import EventSettingsModal from './EventSettingsModal'
+
+storiesOf('FoodFleet|event/EventSettingsModal', module)
+  .addParameters({
+    backgrounds: [
+      { name: 'default', value: '#f1f3f6', default: true }
+    ]
+  })
+  .add('default', () => ({
+    components: { EventSettingsModal },
+    methods: {
+      save () {
+        action('save')()
+      },
+      cancel () {
+        action('cancel')()
+      }
+    },
+    template: `
+      <event-settings-modal
+        @save="save"
+        @cancel="cancel"
+      />
+    `
+  }))
---
+++ resources/js/components/events/EventSettingsModal.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <div>
+      <v-checkbox
+        v-model="isChecked"
+        label="Recurring Event"
+      />
+    </div>
+
+    <div :class="isChecked ? 'enable-area' : 'disable-area'">
+      <div class="caption font-weight-bold grey--text">
+        REPEAT EVERY
+      </div>
+      <a
+        v-if="!selectedDate"
+        class="caption"
+        @click="selectDate"
+      >
+        Select date
+      </a>
+      <a
+        v-else
+        class="caption"
+        @click="selectDate"
+      >
+        {{ selectedDate }}
+      </a>
+    </div>
+
+    <EventSettings
+      ref="form"
+      @save="save"
+      @cancel="cancel"
+    />
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+import EventSettings from './EventSettings'
+
+export default {
+  name: 'EventSettingsModal',
+  components: {
+    EventSettings
+  },
+  data () {
+    return {
+      isChecked: false,
+      selectedDate: '',
+      formData: []
+    }
+  },
+  methods: {
+    selectDate () {
+      this.$refs.form.isDialogOpened = true
+    },
+    save (formData) {
+      this.formData = formData
+
+      const currentDate = moment()
+
+      switch (formData.intervalUnit) {
+        case 'Week(s)':
+          const weekDays = formData.repeatOn.map(v => v.text)
+          if (formData.endsOn === 'after') {
+            const daysLeft = formData.occurrences * formData.intervalValue
+            const endDate = currentDate.add(daysLeft, 'd')
+
+            this.selectedDate = `${weekDays.join(', ')}, until ${endDate.format('MMMM Do, YYYY')}`
+          } else {
+            this.selectedDate = `Every ${weekDays.join(', ')}`
+          }
+          break
+
+        case 'Month(s)':
+          let description
+          if (formData.repeatOn.length >= 0) {
+            description = formData.repeatOn[0].text
+          }
+          if (formData.endsOn === 'after') {
+            const monthsLeft = formData.occurrences * formData.intervalValue
+            const endDate = currentDate.add(monthsLeft, 'M')
+
+            this.selectedDate = `${description}, util ${endDate.format('MMMM Do, YYYY')}`
+          } else {
+            this.selectedDate = `${description}`
+          }
+          break
+
+        case 'Year(s)':
+          if (formData.endsOn === 'after') {
+            const yearsLeft = formData.occurrences * formData.intervalValue
+            const endDate = currentDate.add(yearsLeft, 'Y')
+
+            this.selectedDate = `Every ${formData.intervalValue} year(s),
+              until ${endDate.format('MMMM Do, YYYY')}`
+          } else {
+            this.selectedDate = `Every ${formData.intervalValue} year(s)`
+          }
+          break
+
+        default:
+          break
+      }
+    },
+    cancel () {
+      this.formData = []
+      this.selectedDate = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.disable-area {
+  pointer-events: none;
+  opacity: 0.5;
+}
+.disable-area a {
+  color: grey;
+}
+.enable-area a {
+  text-decoration: underline;
+}
+</style>
