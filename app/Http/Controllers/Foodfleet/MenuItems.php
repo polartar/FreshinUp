ae19ee6b1c304cf99996d8716715989d83e874bb
kevin <kevinc@freshinup.com>
2019-12-13 15:25:34 +0000

    Menu Items API added

--- app/Http/Controllers/Foodfleet/MenuItems.php
+++ app/Http/Controllers/Foodfleet/MenuItems.php
@@ -23,4 +23,80 @@ class MenuItems extends Controller
             ]);
         return MenuItemResource::collection($items->get());
     }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'store_uuid' => 'string|required|exists:stores,uuid',
+            'title' => 'string|required',
+            'servings' => 'integer|required',
+            'cost' => 'integer|required',
+            'description' => 'string'
+        ]);
+
+        $inputs = $request->input();
+        $menuItem = MenuItem::create($inputs);
+
+        return new \App\Http\Resources\Foodfleet\MenuItem($menuItem);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request,$store_uuid, $uuid)
+    {
+        $menu = QueryBuilder::for(MenuItem::class, $request)
+            ->where('uuid', $uuid)
+            ->allowedIncludes([ 'store'])
+            ->firstOrFail();
+
+        return new MenuItemResource($menu);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $store_uuid, $uuid)
+    {
+        $this->validate($request, [
+            'title' => 'string',
+            'servings' => 'integer',
+            'cost' => 'integer',
+            'description' => 'string'
+        ]);
+
+        $inputs = $request->input();
+        $menuItem = MenuItem::where('uuid', $uuid)->first();
+        if ($menuItem) {
+            $menuItem->update($inputs);
+        }
+        return new MenuItemResource($menuItem);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($store_uuid,$uuid)
+    {
+        $menuItem = MenuItem::where('uuid', $uuid)->firstOrFail();
+        $menuItem->delete();
+        return response()->json(null, SymfonyResponse::HTTP_NO_CONTENT);
+    }
 }
