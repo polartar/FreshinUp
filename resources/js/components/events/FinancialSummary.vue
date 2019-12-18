2a3a64dead6ff63b3ee6386c0ad7b5cfcb5b8786
codevon <hngliang@gmail.com>
2019-12-18 18:23:42 +0800

    fixed unit test & lint issues

--- resources/js/components/events/FinancialSummary.vue
+++ resources/js/components/events/FinancialSummary.vue
@@ -1,29 +1,84 @@
 <template>
   <v-card>
-    <v-container font-weight-bold grey--text subheading py-3>
+    <v-container
+      font-weight-bold
+      grey--text
+      subheading
+      py-3
+    >
       Event Financial Summary
     </v-container>
-    <v-divider/>
-    <v-container pt-0 font-weight-bold grey--text>
-      <v-layout align-center line>
-        <v-flex caption font-weight-bold>TOTAL FLEET</v-flex>
-        <v-flex text-xs-right subheading xs6 text-truncate>{{financial.total_fleet}}</v-flex>
+    <v-divider />
+    <v-container
+      pt-0
+      font-weight-bold
+      grey--text
+    >
+      <v-layout
+        align-center
+        line
+      >
+        <v-flex
+          caption
+          font-weight-bold
+        >
+          TOTAL FLEET
+        </v-flex>
+        <v-flex
+          text-xs-right
+          subheading
+          xs6
+          text-truncate
+        >
+          {{ financial.total_fleet }}
+        </v-flex>
       </v-layout>
-      <v-divider/>
-      <v-layout align-center line>
-        <v-flex caption font-weight-bold>TOTAL COST</v-flex>
-        <v-flex text-xs-right subheading xs6 text-truncate>{{financial.total_cost}}</v-flex>
+      <v-divider />
+      <v-layout
+        align-center
+        line
+      >
+        <v-flex
+          caption
+          font-weight-bold
+        >
+          TOTAL COST
+        </v-flex>
+        <v-flex
+          text-xs-right
+          subheading
+          xs6
+          text-truncate
+        >
+          {{ financial.total_cost }}
+        </v-flex>
       </v-layout>
-      <v-divider/>
-      <v-layout align-center line>
-        <v-flex caption font-weight-bold>AMOUNT DUE</v-flex>
-        <v-flex text-xs-right subheading xs6 text-truncate>{{financial.amount_due}}</v-flex>
+      <v-divider />
+      <v-layout
+        align-center
+        line
+      >
+        <v-flex
+          caption
+          font-weight-bold
+        >
+          AMOUNT DUE
+        </v-flex>
+        <v-flex
+          text-xs-right
+          subheading
+          xs6
+          text-truncate
+        >
+          {{ financial.amount_due }}
+        </v-flex>
       </v-layout>
-      <v-divider/>
+      <v-divider />
 
-      <v-btn block
-             class="grey ma-0 mt-4 white--text text-none"
-             @click="onButtonClick"
+      <v-btn
+        block
+        class="grey ma-0 mt-4 white--text text-none"
+        @click="onButtonClick"
       >
         View Contract
       </v-btn>
