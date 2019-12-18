fb8f2c3215b98f23a4ba10380466c27790c2ee6f
codevon <hngliang@gmail.com>
2019-12-18 18:00:40 +0800

    fixed lint issues

--- resources/js/components/events/CustomerSummary.vue
+++ resources/js/components/events/CustomerSummary.vue
@@ -1,34 +1,104 @@
 <template>
   <v-card>
-    <v-container font-weight-bold grey--text subheading py-3>
+    <v-container
+      font-weight-bold
+      grey--text
+      subheading
+      py-3
+    >
       Customer Summary
     </v-container>
-    <v-divider/>
-    <v-container pt-0 font-weight-bold grey--text>
-      <v-layout align-center line>
-        <v-flex caption font-weight-bold>OWNER</v-flex>
-        <v-flex text-xs-right subheading xs6 text-truncate>{{customer.owner}}</v-flex>
+    <v-divider />
+    <v-container
+      pt-0
+      font-weight-bold
+      grey--text
+    >
+      <v-layout
+        align-center
+        line
+      >
+        <v-flex
+          caption
+          font-weight-bold
+        >
+          OWNER
+        </v-flex>
+        <v-flex
+          text-xs-right
+          subheading
+          xs6
+          text-truncate
+        >
+          {{ customer.owner }}
+        </v-flex>
       </v-layout>
-      <v-divider/>
-      <v-layout align-center line>
-        <v-flex caption font-weight-bold>SIGNED CONTRACTS</v-flex>
-        <v-flex text-xs-right subheading xs6 text-truncate>{{customer.signed_contracts}}</v-flex>
+      <v-divider />
+      <v-layout
+        align-center
+        line
+      >
+        <v-flex
+          caption
+          font-weight-bold
+        >
+          SIGNED CONTRACTS
+        </v-flex>
+        <v-flex
+          text-xs-right
+          subheading
+          xs6
+          text-truncate
+        >
+          {{ customer.signed_contracts }}
+        </v-flex>
       </v-layout>
-      <v-divider/>
-      <v-layout align-center line>
-        <v-flex caption font-weight-bold>CONTACT PHONE</v-flex>
-        <v-flex text-xs-right subheading xs6 text-truncate>{{customer.phone}}</v-flex>
+      <v-divider />
+      <v-layout
+        align-center
+        line
+      >
+        <v-flex
+          caption
+          font-weight-bold
+        >
+          CONTACT PHONE
+        </v-flex>
+        <v-flex
+          text-xs-right
+          subheading
+          xs6
+          text-truncate
+        >
+          {{ customer.phone }}
+        </v-flex>
       </v-layout>
-      <v-divider/>
-      <v-layout align-center line>
-        <v-flex caption font-weight-bold>CONTACT EMAIL</v-flex>
-        <v-flex text-xs-right subheading xs6 text-truncate>{{customer.email}}</v-flex>
+      <v-divider />
+      <v-layout
+        align-center
+        line
+      >
+        <v-flex
+          caption
+          font-weight-bold
+        >
+          CONTACT EMAIL
+        </v-flex>
+        <v-flex
+          text-xs-right
+          subheading
+          xs6
+          text-truncate
+        >
+          {{ customer.email }}
+        </v-flex>
       </v-layout>
-      <v-divider/>
+      <v-divider />
 
-      <v-btn block
-             class="grey ma-0 mt-4 white--text text-none"
-             @click="onButtonClick"
+      <v-btn
+        block
+        class="grey ma-0 mt-4 white--text text-none"
+        @click="onButtonClick"
       >
         View Customer Profile
       </v-btn>
