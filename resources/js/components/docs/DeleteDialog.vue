816267aef3f6f26a5881dd70c6f26ef1c4e7ff6e
Matt Palermo <matt@freshinup.com>
2020-01-02 12:33:40 -0500

    Extract docs datatable management into reusuable mixin

---
+++ resources/js/components/docs/DeleteDialog.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-dialog
+    v-bind="$attrs"
+    max-width="500"
+    v-on="$listeners"
+  >
+    <simple-confirm
+      :class="{ deleting: processing }"
+      :title="dialogTitle"
+      ok-label="Yes"
+      cancel-label="No"
+      @ok="onSubmitDelete"
+      @cancel="onCancelDelete"
+    >
+      <div class="py-5 px-2">
+        <template v-if="processing">
+          <div class="text-xs-center">
+            <p class="subheading">
+              Processing, please wait...
+            </p>
+            <v-progress-circular
+              :rotate="-90"
+              :size="200"
+              :width="15"
+              :value="deletablesProgress"
+              color="primary"
+            >
+              {{ deletablesStatus }}
+            </v-progress-circular>
+          </div>
+        </template>
+        <template v-else>
+          <p class="subheading">
+            <span v-if="deletables.length < 2">Document</span>
+            <span v-else>Documents</span>
+            : {{ deleteTemp | formatDeleteTitles }}
+          </p>
+        </template>
+      </div>
+    </simple-confirm>
+  </v-dialog>
+</template>
+
+<script>
+import { deletables } from 'fresh-bus/components/mixins/Deletables'
+import simpleConfirm from 'fresh-bus/components/SimpleConfirm.vue'
+export default {
+  components: {
+    simpleConfirm
+  },
+  filters: {
+    formatDeleteTitles (value) {
+      return value.map(item => item.title).join(', ')
+    }
+  },
+  mixins: [deletables],
+  props: {
+    processing: {
+      type: Boolean,
+      default: false
+    },
+    dialogTitle: {
+      type: String,
+      default: 'Are you sure you want to delete the selected item(s)'
+    },
+    deleteTemp: {
+      type: Array,
+      default: () => []
+    },
+    onSubmitDelete: {
+      type: Function,
+      required: true
+    },
+    onCancelDelete: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
---
+++ resources/js/components/mixins/DocsDatatableManager.js
@@ -0,0 +1,137 @@
+import { mapGetters, mapState } from 'vuex'
+import { deletables } from 'fresh-bus/components/mixins/Deletables'
+const statusesColored = (statuses, statusColorMap) => {
+  if (!statuses.length) return statuses
+  for (let i in statuses) {
+    statuses[i].color = statusColorMap[statuses[i].value]
+  }
+  return statuses
+}
+export default {
+  data () {
+    return {
+      docsDeleteDialog: false,
+      docsLastFilterParams: {
+        sort: '-created_at'
+      },
+      docsDeleteTemp: []
+    }
+  },
+  computed: {
+    ...mapGetters('documentStatuses', { docsStatuses: 'items' }),
+    ...mapState('documents', { docsSortables: 'sortables' }),
+    ...mapGetters('documents', {
+      docs: 'items',
+      docsPagination: 'pagination',
+      docsSorting: 'sorting',
+      docsSortBy: 'sortBy'
+    }),
+
+    docsActiveStoreUuid () {
+      return this.$route.params.id
+    },
+
+    docsStatusesWithColors () {
+      return statusesColored(JSON.parse(JSON.stringify(this.docsStatuses)), {
+        1: 'warning',
+        2: 'success',
+        3: 'danger',
+        4: 'secondary',
+        5: 'secondary'
+      })
+    }
+  },
+  mixins: [deletables],
+  methods: {
+    docsNew () {
+      this.$router.push({ path: '/admin/docs/new' })
+    },
+    docsView (doc) {
+      this.$router.push({ path: '/admin/docs/' + doc.uuid })
+    },
+    docsEdit (doc) {
+      this.$router.push({ path: '/admin/docs/' + doc.uuid })
+    },
+    docsDelete (docs) {
+      if (!Array.isArray(docs)) {
+        docs = [docs]
+      }
+      this.docsDeleteTemp = docs
+      this.docsDeleteDialog = true
+    },
+    async docsOnSubmitDelete () {
+      this.deletablesProcessing = true
+      this.deletablesProgress = 0
+      this.deletablesStatus = ''
+      let dispatcheables = []
+
+      this.docsDeleteTemp.forEach(doc => {
+        dispatcheables.push(
+          this.$store.dispatch('documents/deleteItem', {
+            getItems: false,
+            params: { id: doc.uuid }
+          })
+        )
+      })
+
+      let chunks = this.chunk(
+        dispatcheables,
+        this.docsDeleteTempParrallelRequest
+      )
+      let doneCount = 0
+
+      for (let i in chunks) {
+        await Promise.all(chunks[i])
+        doneCount += chunks[i].length
+        this.docsDeleteTempStatus =
+          doneCount + ' / ' + this.docsDeleteTemp.length + ' Done'
+        this.docsDeleteTempProgress =
+          (doneCount / this.docsDeleteTemp.length) * 100
+        await this.sleep(this.docsDeletablesSleepTime)
+      }
+
+      this.docsFilter(this.docsLastFilterParams)
+      await this.sleep(500)
+      this.docsDeletablesProcessing = false
+      this.docsDeleteDialog = false
+    },
+    docsOnCancelDelete () {
+      this.docsDeleteDialog = false
+      this.docsDeleteTemp = []
+    },
+
+    docsOnPaginate (value) {
+      this.$store.dispatch('documents/setPagination', value)
+      this.$store.dispatch('documents/getItems', {
+        params: {
+          'filter[assigned_uuid]': this.docsActiveStoreUuid
+        }
+      })
+    },
+    docsChangeStatus (status, doc) {
+      this.$store
+        .dispatch('documents/patchItem', {
+          data: { status },
+          params: { id: doc.uuid }
+        })
+        .then(() => {
+          this.docsFilter(this.docsLastFilterParams)
+        })
+    },
+    docsChangeStatusMultiple (status, docs) {
+      docs.forEach(doc => {
+        this.changeStatus(status, doc)
+      })
+    },
+
+    docsFilter (params) {
+      this.$store.dispatch('documents/setSort', params.sort)
+      this.$store.dispatch('documents/setFilters', params)
+      this.$store.dispatch('documents/getItems', {
+        params: {
+          'filter[assigned_uuid]': this.docsActiveStoreUuid
+        }
+      })
+    }
+  }
+}
