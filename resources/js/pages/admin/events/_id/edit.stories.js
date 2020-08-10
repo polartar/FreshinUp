d603c0d1fbf49336eefa3cd9d30f3f87b3c9f68a
Babacar Cissé DIA <bcdbuddy@outlook.com>
2020-08-10 22:01:19 +0000

    create event edit stories

---
+++ resources/js/pages/admin/events/_id/edit.stories.js
@@ -0,0 +1,74 @@
+import createStore from 'tests/createStore'
+import makePageStory from 'vue-cli-plugin-freshinup-ui/utils/makePageStory'
+import { MAIN } from '../../../../../../.storybook/categories.js'
+import { FIXTURE_USER } from 'tests/__data__/user'
+import { FIXTURE_FINANCIAL_SEARCHES } from 'tests/__data__/financialSavedReports'
+import Page from './edit.vue'
+import PageStoryInstances from 'tests/PageStoryInstances'
+
+export default {
+  title: `${MAIN} Pages|admin/events/edit`,
+  id: 'pages/admin/events/edit'
+}
+
+export const IsLoadingPage = () => {
+  const store = createStore({
+    currentUser: FIXTURE_USER
+  })
+  return makePageStory(Page, store, {
+    ...PageStoryInstances,
+    apiMockRoutes: [
+      {
+        path: 'api/currentUser',
+        GET: [200, FIXTURE_USER]
+      }
+    ],
+    beforeMount () {
+      store.dispatch('page/setLoading', true)
+    }
+  })
+}
+
+export const PopulatedList = () => {
+  const store = createStore({
+    currentUser: FIXTURE_USER
+  })
+  return makePageStory(Page, store, {
+    ...PageStoryInstances,
+    apiMockRoutes: [
+      {
+        path: /.*api\/financial-reports.*/,
+        GET: [200, { data: FIXTURE_FINANCIAL_SEARCHES.slice(0, 10) }]
+      },
+      {
+        path: 'api/currentUser',
+        GET: [200, FIXTURE_USER]
+      }
+    ],
+    beforeMount () {
+      Page.beforeRouteEnterOrUpdate(this)
+    }
+  })
+}
+
+export const EmptyList = () => {
+  const store = createStore({
+    currentUser: FIXTURE_USER
+  })
+  return makePageStory(Page, store, {
+    ...PageStoryInstances,
+    apiMockRoutes: [
+      {
+        path: /.*api\/finacial-reports.*/,
+        GET: [200, { data: [] }]
+      },
+      {
+        path: 'api/currentUser',
+        GET: [200, FIXTURE_USER]
+      }
+    ],
+    beforeMount () {
+      Page.beforeRouteEnterOrUpdate(this)
+    }
+  })
+}
