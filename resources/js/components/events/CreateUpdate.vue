1c8d560ae703e82f33f3d0c219a826dbb29a9b4e
Babacar Cissé DIA <bcdbuddy@outlook.com>
2020-08-18 13:02:31 +0000

    update layout

--- resources/js/components/events/CreateUpdate.vue
+++ resources/js/components/events/CreateUpdate.vue
@@ -135,68 +135,37 @@
           sm2
           xs12
         >
-          <status-select
-            v-model="status_id"
-            :options="statuses"
-          />
-        </v-flex>
-        <v-layout
-          row
-          align-center
-        >
-          <v-dialog
-            v-model="questDialog"
-            max-width="500"
+          <v-layout
+            align-center
           >
-            <template v-slot:activator="{ on }">
-              <v-btn
-                depressed
-                fab
-                color="primary"
-                @click="questDialog = true"
-              >
-                <v-icon>far fa-question-circle</v-icon>
-              </v-btn>
-            </template>
-            <v-card>
-              <v-card-title>
-                <v-layout
-                  row
-                  space-between
-                  align-center
+            <status-select
+              v-model="status_id"
+              :options="statuses"
+            />
+            <v-dialog
+              v-model="questDialog"
+              max-width="500"
+            >
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  depressed
+                  fab
+                  color="primary"
+                  @click="questDialog = true"
                 >
-                  <v-flex>
-                    <h3></h3>
-                  </v-flex>
-                  <v-btn
-                    small
-                    round
-                    color="grey"
-                    class="white--text"
-                    @click="questDialog = false"
-                  >
-                    <v-flex>
-                      <v-icon
-                        small
-                        class="white--text"
-                      >
-                        fa fa-times
-                      </v-icon>
-                    </v-flex>
-                    <v-flex>
-                      Close
-                    </v-flex>
-                  </v-btn>
-                </v-layout>
-              </v-card-title>
-              <v-divider />
-              <v-card-text class="grey--text">
-                <p>Coming Soon</p>
-              </v-card-text>
-              <v-divider />
-            </v-card>
-          </v-dialog>
-        </v-layout>
+                  <v-icon>far fa-question-circle</v-icon>
+                </v-btn>
+              </template>
+              <v-card>
+                <v-divider />
+                <v-card-text class="grey--text">
+                  <p>Coming Soon</p>
+                </v-card-text>
+                <v-divider />
+              </v-card>
+            </v-dialog>
+          </v-layout>
+        </v-flex>
       </v-flex>
       <v-divider />
       <br>
