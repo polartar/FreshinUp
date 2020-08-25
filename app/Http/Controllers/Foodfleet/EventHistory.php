cef2d2067341cb28b72bbd3b3151cc404efc3cd0
Babacar Cissé DIA <bcdbuddy@outlook.com>
2020-08-25 18:24:23 +0000

    add todo as comment in code

--- app/Http/Controllers/Foodfleet/EventHistory.php
+++ app/Http/Controllers/Foodfleet/EventHistory.php
@@ -24,6 +24,23 @@ class EventHistory extends Controller
                 Filter::exact('event_uuid')
             ])->get();
 
+        // TODO: should return a list of all statuses, not just the current status changes
+        // if filter event_uuid is detected then apply this logic
+        // $event_uuid = $request->query('event_uuid');
+        // for status in statuses
+
+//        $histories = [
+//            [
+//                'id' => 1,
+//                'event_uuid' => $event_uuid,
+//                'status_id' => $status->id,
+//                'name' => $status->name,
+//                'completed' => // depend on whether or not the status has been found and completed,
+//                'date' => // $history->date  ?? '',
+//                'description' => // $history->description ?? getEventStatusDescription
+//            ]
+//        ];
+
         return EventHistoryResource::collection($eventHistories);
     }
 }
