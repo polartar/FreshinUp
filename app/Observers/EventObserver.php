77626e0edec13c102846eaafffa7f92776b662bd
Babacar Cissé DIA <bcdbuddy@outlook.com>
2020-09-07 01:33:34 +0000

    event history: wrap backend

---
+++ app/Observers/EventObserver.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Foodfleet\Event;
+use App\Models\Foodfleet\EventHistory;
+
+class EventObserver
+{
+    /**
+     * Handle the event "created" event.
+     *
+     * @param  Event  $event
+     * @return void
+     */
+    public function created(Event $event)
+    {
+        EventHistory::create([
+            'event_uuid' => $event->uuid,
+            'status_id' => $event->status_id,
+        ]);
+    }
+
+    /**
+     * Handle the event "updated" event.
+     *
+     * @param  Event  $event
+     * @return void
+     */
+    public function updated(Event $event)
+    {
+        EventHistory::firstOrCreate([
+            'event_uuid' => $event->uuid,
+            'status_id' => $event->status_id,
+        ]);
+    }
+
+    /**
+     * Handle the event "deleted" event.
+     *
+     * @param  Event  $event
+     * @return void
+     */
+    public function deleted(Event $event)
+    {
+        EventHistory::where('event_uuid', $event->uuid)->delete();
+    }
+
+    /**
+     * Handle the event "restored" event.
+     *
+     * @param  Event  $event
+     * @return void
+     */
+    public function restored(Event $event)
+    {
+        //
+    }
+
+    /**
+     * Handle the event "force deleted" event.
+     *
+     * @param  Event  $event
+     * @return void
+     */
+    public function forceDeleted(Event $event)
+    {
+        //
+    }
+}
