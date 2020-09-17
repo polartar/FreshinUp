4b52c81f0d95587d5763448622e695ee13041a32
eliusxpol <eliusxpol@hotmail.com>
2020-09-17 05:40:58 +0000

    New Venue page init

---
+++ resources/js/pages/admin/venues/new.vue
@@ -0,0 +1,14 @@
+<script>
+import CreateUpdate from '../../../components/venues/CreateUpdate'
+
+export default {
+  extends: CreateUpdate,
+  layout: CreateUpdate.layout,
+  beforeRouteEnterOrUpdate: CreateUpdate.beforeRouteEnterOrUpdate,
+  data () {
+    return {
+      isNew: true
+    }
+  }
+}
+</script>
