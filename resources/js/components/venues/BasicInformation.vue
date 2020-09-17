55ea27a937d0d9897397d24d97251e2360039779
eliusxpol <eliusxpol@hotmail.com>
2020-09-17 05:42:00 +0000

    Venue BasicInformation component init

---
+++ resources/js/components/venues/BasicInformation.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Basic Information
+      <v-progress-linear
+        v-if="loading"
+        indeterminate
+      />
+    </v-card-title>
+    <v-divider />
+    <v-layout pa-3>
+      Contenu
+    </v-layout>
+    <v-divider />
+    <v-card-actions class="pa-3 d-flex justify-space-between">
+      Actions buttons
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    loading: { type: Boolean, default: false }
+  }
+}
+</script>
