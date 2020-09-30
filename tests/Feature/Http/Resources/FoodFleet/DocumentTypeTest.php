3b2c77446d2185eb4fa7d5c7ab9835620b386cae
mbayane <hannmbaye@gmail.com>
2020-09-30 06:03:20 -0400

    change resource Document?Type to DocumentType and update test resource

--- tests/Feature/Http/Resources/FoodFleet/DocumentTypeTest.php
+++ tests/Feature/Http/Resources/FoodFleet/DocumentTypeTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature\Http\Resources\Foodfleet;
 
-use App\Http\Resources\Foodfleet\Document\Type as DocumentTypeResource;
+use App\Http\Resources\Foodfleet\DocumentType as DocumentTypeResource;
 use App\Models\Foodfleet\DocumentType as DocumentTypeModel;
 use Illuminate\Http\Request;
 use Tests\TestCase;
@@ -13,6 +13,8 @@ class DocumentTypeTest extends TestCase {
         $documentType = factory(DocumentTypeModel::class)->make();
         $resource = new DocumentTypeResource($documentType);
         $expected = [
+            'id' => $documentType->id,
+            'name' => $documentType->name,
             'value' => $documentType->id,
             'text' => $documentType->name,
         ];
