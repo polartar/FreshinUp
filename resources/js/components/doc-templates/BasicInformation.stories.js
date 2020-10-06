610267819af7b80a72ea004f6753ceed93e019c6
eliusxpol <eliusxpol@hotmail.com>
2020-10-06 00:49:16 +0000

    Doc template initial basic information with stories

---
+++ resources/js/components/doc-templates/BasicInformation.stories.js
@@ -0,0 +1,31 @@
+import { storiesOf } from '@storybook/vue'
+// import { action } '@storybook/addon-actions'
+
+import BasicInformation from './BasicInformation'
+
+export const Default = () => ({
+  components: { BasicInformation },
+  template: `
+    <v-container>
+      <basic-information/>
+    </v-container>
+  `
+})
+
+export const Loading = () => ({
+  components: { BasicInformation },
+  template: `
+    <v-container>
+      <basic-information loading/>
+    </v-container>
+  `
+})
+
+storiesOf('FoodFleet|components/doc-templates/BasicInformation', module)
+  .addParameters({
+    backgrounds: [
+      { name: 'default', value: '#f1f3f6', default: true }
+    ]
+  })
+  .add('Default', Default)
+  .add('Loading', Loading)
---
+++ resources/js/components/doc-templates/BasicInformation.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Basic information doc template
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
