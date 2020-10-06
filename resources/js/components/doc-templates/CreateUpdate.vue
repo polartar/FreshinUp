c5ba8eb06d0fafe98608a2580aca41885d46e454
eliusxpol <eliusxpol@hotmail.com>
2020-10-06 00:34:58 +0000

    Init Create or update doc template

---
+++ resources/js/components/doc-templates/CreateUpdate.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Create Update doc template view
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'admin',
+
+  methods: {
+    beforeRouteEnterOrUpdate (vm, to, from, next) {
+      vm.$store.dispatch('page/setLoading', false)
+      next && next()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
