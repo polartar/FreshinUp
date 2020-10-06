7504924356948e6756baee21303b8c44a4cdbe76
eliusxpol <eliusxpol@hotmail.com>
2020-10-06 01:02:15 +0000

    Adding tilte and short description field

--- resources/js/components/doc-templates/BasicInformation.vue
+++ resources/js/components/doc-templates/BasicInformation.vue
@@ -1,11 +1,45 @@
 <template>
-  <div>
-    Basic information doc template
-  </div>
+  <v-card>
+    <v-card-title>
+      <h3 class="grey--text">
+        Basic Information
+      </h3>
+      <v-progress-linear
+        v-if="loading"
+        indeterminate
+      />
+    </v-card-title>
+    <v-divider />
+    <div class="pa-3">
+      <v-flex xs12>
+        <div class="mb-2 text-uppercase grey--text font-weight-bold">
+          Title
+        </div>
+        <v-text-field
+          placeholder="Title"
+          single-line
+          outline
+        />
+      </v-flex>
+      <v-flex xs12>
+        <div class="mb-2 text-uppercase grey--text font-weight-bold">
+          Short description
+        </div>
+        <v-textarea
+          placeholder="Short description"
+          single-line
+          outline
+        />
+      </v-flex>
+    </div>
+  </v-card>
 </template>
 
 <script>
 export default {
+  props: {
+    loading: { type: Boolean, default: false }
+  }
 }
 </script>
 
