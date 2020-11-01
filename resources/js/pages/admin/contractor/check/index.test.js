8ec95bb77b6e1aae41f66361bea3ea07b8855c25
Babacar Cissé DIA <bcdbuddy@outlook.com>
2020-11-01 14:35:44 +0000

    wrap #522

--- resources/js/pages/admin/contractor/check/index.test.js
+++ resources/js/pages/admin/contractor/check/index.test.js
@@ -1,33 +1,26 @@
-import { mount } from '@vue/test-utils'
 import createLocalVue from 'vue-cli-plugin-freshinup-ui/utils/testing/createLocalVue'
 import find from 'lodash/find'
-import Component from '~/pages/admin/contractor/check/index.vue'
+import Page from './index.vue'
 import createStore from 'tests/createStore'
-
-describe('Contractor check authorization page', () => {
-  let localVue, mock
+import { mount } from 'vue-cli-plugin-freshinup-ui/utils/testing'
+describe('pages/admin/contractor/check', () => {
+  let localVue, mock, store
   describe('Scenarios', () => {
     beforeEach(() => {
       const vue = createLocalVue({ validation: true })
       localVue = vue.localVue
       mock = vue.mock
+      store = createStore()
     })
     afterEach(() => {
       mock.restore()
     })
-    it.skip('the code is correct', (done) => {
-      const vue = createLocalVue({ validation: true })
-      localVue = vue.localVue
-      mock = vue.mock
-        .onPost('api/foodfleet/squares/authorize').reply(201, [])
-        .onAny()
-        .reply(config => {
-          console.warn('No mock match for ' + config.url, config)
-          return [404, {}]
-        })
-      const store = createStore()
-      const wrapper = mount(Component, {
-        localVue: localVue,
+    test.skip('when the code is correct', (done) => {
+      mock.onPost(/.*foodfleet\/squares\/authorize.*/)
+        .reply(200, {})
+
+      const wrapper = mount(Page, {
+        localVue,
         store,
         mocks: {
           $route: {
@@ -38,25 +31,18 @@ describe('Contractor check authorization page', () => {
         }
       })
 
-      Component.beforeRouteEnterOrUpdate(wrapper.vm, null, null, () => {
+      Page.beforeRouteEnterOrUpdate(wrapper.vm, null, null, () => {
         const request = find(mock.history.post, (req) => req.url === 'api/foodfleet/squares/authorize')
         expect(request.data).toBe('{"code":"sandbox-sq0idb-XpOEfx_tSDS3oIDPuf9Tqw"}')
         expect(wrapper.vm.result).toBe(true)
         done()
       })
     })
-    it.skip('the code is not correct', (done) => {
-      const vue = createLocalVue({ validation: true })
-      localVue = vue.localVue
-      mock = vue.mock
-        .onPost('api/foodfleet/squares/authorize').reply(400, [])
-        .onAny()
-        .reply(config => {
-          console.warn('No mock match for ' + config.url, config)
-          return [404, {}]
-        })
-      const store = createStore()
-      const wrapper = mount(Component, {
+    test.skip('when the code is not correct', (done) => {
+      mock.onPost(/.*foodfleet\/squares\/authorize.*/)
+        .reply(200, { result: false })
+
+      const wrapper = mount(Page, {
         localVue: localVue,
         store,
         mocks: {
@@ -68,7 +54,7 @@ describe('Contractor check authorization page', () => {
         }
       })
 
-      Component.beforeRouteEnterOrUpdate(wrapper.vm, null, null, () => {
+      Page.beforeRouteEnterOrUpdate(wrapper.vm, null, null, () => {
         const request = find(mock.history.post, (req) => req.url === 'api/foodfleet/squares/authorize')
         expect(request.data).toBe('{"code":"sandbox-sq0idb-XpOEfx_tSDS3oIDPuf9Tqw"}')
         expect(wrapper.vm.result).toBe(false)
