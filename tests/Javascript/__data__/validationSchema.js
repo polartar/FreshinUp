22c80b2ff26ce3572b5fbcce880f104b59b07e69
ben <ben.lin0025@gmail.com>
2020-11-03 09:25:50 -0500

    create validation schema

---
+++ tests/Javascript/__data__/validationSchema.js
@@ -0,0 +1,16 @@
+export const FIXTURE_VALIDATION_SCHEMA = {
+  template: [
+    'id',
+    'uuid',
+    'title',
+    'description',
+    'content',
+    'status_id',
+    'updated_by_uuid',
+    'created_at',
+    'updated_at',
+    'updateBy',
+    'status',
+    'documents'
+  ]
+}
