e40a42967b105d2f781481957d0e6e717f379365
james.conway <jamesconway325@gmail.com>
2020-11-19 02:25:47 -0800

    fix options in user status in userlist

--- database/seeds/StatusesSeeder.php
+++ database/seeds/StatusesSeeder.php
@@ -14,13 +14,14 @@ class StatusesSeeder extends Seeder
      */
     public function run()
     {
+        UserStatus::truncate();
+           
         $statuses = [
-            1 => 'Active',
-            2 => 'Idle',
-            3 => 'On Hold',
-            4 => 'Pending',
-            5 => 'Prospect/Lead',
-            6 => 'New',
+            1 => 'Prospect / Lead',
+            2 => 'Pending Invitation',
+            3 => 'Pending Review',
+            4 => 'Approved',
+            5 => 'On Hold',
         ];
 
         foreach($statuses as $id => $name) {
