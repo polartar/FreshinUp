a6b204d4ab25c7ccaf16504ebeb88084bfd7047f
james.conway <jamesconway325@gmail.com>
2020-11-20 06:26:49 -0800

    added back button in user detail page

--- resources/js/pages/admin/users/_id/edit.vue
+++ resources/js/pages/admin/users/_id/edit.vue
@@ -1,7 +1,31 @@
+<template>
+  <div>
+    <v-layout
+      row
+      align-center
+      pt-3
+    >
+      <v-btn
+        flat
+        small
+        @click="backToList"
+      >
+        <div class="back-btn-inner">
+          <v-icon>fas fa-arrow-left</v-icon>
+          <span>Return to User list</span>
+        </div>
+      </v-btn>
+    </v-layout>
+
+    <create-update />
+  </div>
+</template>
+
 <script>
 import CreateUpdate from 'fresh-bus/components/pages/admin/users/CreateUpdate.vue'
 
 export default {
+  components: { CreateUpdate },
   extends: CreateUpdate,
   layout: CreateUpdate.layout,
   beforeRouteEnterOrUpdate: CreateUpdate.beforeRouteEnterOrUpdate,
@@ -22,6 +46,28 @@ export default {
         'post_code'
       ]
     }
+  },
+  methods: {
+    backToList () {
+      this.$router.push({ path: '/admin/users' })
+    }
   }
 }
 </script>
+
+<style scoped>
+  .back-btn-inner{
+    color: #fff;
+    display: flex;
+    align-items: center;
+    font-size: 13px;
+  }
+  .back-btn-inner span{
+    margin-left: 10px;
+    font-weight: bold;
+    text-transform: initial;
+  }
+  .back-btn-inner .v-icon{
+    font-size: 16px;
+  }
+</style>
