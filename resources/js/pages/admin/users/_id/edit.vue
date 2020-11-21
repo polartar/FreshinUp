5d6f21545311231192168b6271a7affe3377927f
james.conway <jamesconway325@gmail.com>
2020-11-20 17:33:12 -0800

    added return button in users detail page

--- resources/js/pages/admin/users/_id/edit.vue
+++ resources/js/pages/admin/users/_id/edit.vue
@@ -1,73 +1,9 @@
-<template>
-  <div>
-    <v-layout
-      row
-      align-center
-      pt-3
-    >
-      <v-btn
-        flat
-        small
-        @click="backToList"
-      >
-        <div class="back-btn-inner">
-          <v-icon>fas fa-arrow-left</v-icon>
-          <span>Return to User list</span>
-        </div>
-      </v-btn>
-    </v-layout>
-
-    <create-update />
-  </div>
-</template>
-
 <script>
-import CreateUpdate from 'fresh-bus/components/pages/admin/users/CreateUpdate.vue'
+import CreateUpdate from '~/components/users/CreateUpdate.vue'
 
 export default {
-  components: { CreateUpdate },
   extends: CreateUpdate,
   layout: CreateUpdate.layout,
   beforeRouteEnterOrUpdate: CreateUpdate.beforeRouteEnterOrUpdate,
-  data () {
-    return {
-      enabledFields: [
-        'company',
-        'title',
-        'mobile_phone',
-        'office_phone',
-        'notes',
-        'level',
-        'type',
-        'street',
-        'street_2',
-        'city',
-        'country_id',
-        'post_code'
-      ]
-    }
-  },
-  methods: {
-    backToList () {
-      this.$router.push({ path: '/admin/users' })
-    }
-  }
 }
 </script>
-
-<style scoped>
-  .back-btn-inner{
-    color: #fff;
-    display: flex;
-    align-items: center;
-    font-size: 13px;
-  }
-  .back-btn-inner span{
-    margin-left: 10px;
-    font-weight: bold;
-    text-transform: initial;
-  }
-  .back-btn-inner .v-icon{
-    font-size: 16px;
-  }
-</style>
