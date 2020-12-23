5a0f483c7a9c5d0771d35c9e75b1fd8099f8fd17
talentdev2020 <polaris.center@outlook.com>
2020-12-22 21:24:54 -0700

    Remove Binaryfile and Modify Test/payment.js

--- tests/Javascript/__data__/payments.js
+++ tests/Javascript/__data__/payments.js
@@ -1,6 +1,8 @@
+import { FIXTURE_EVENTS } from './events'
+import { FIXTURE_STORES } from './stores'
 export const FIXTURE_PAYMENTS = [
   {
-    status: 1,
+    status_id: 1,
     event_name: 'Random event name',
     name: 'Event venue fee',
     due_date: '2020-07-08',
@@ -10,7 +12,7 @@ export const FIXTURE_PAYMENTS = [
     description: 'Pariatur et similique mollitia quia nihil'
   },
   {
-    status: 2,
+    status_id: 2,
     event_name: 'Random event name',
     name: 'Event venue fee',
     due_date: '2020-07-08',
@@ -20,7 +22,7 @@ export const FIXTURE_PAYMENTS = [
     description: 'Pariatur et similique mollitia quia nihil'
   },
   {
-    status: 3,
+    status_id: 3,
     event_name: 'Random event name',
     name: 'Event venue fee',
     due_date: '2020-07-08',
@@ -29,6 +31,13 @@ export const FIXTURE_PAYMENTS = [
     venue_due_date: '2020-09-09',
     description: 'Pariatur et similique mollitia quia nihil'
   }
-]
+].map((payment, i) => {
+  return {
+    ...payment,
+    store_uuid: FIXTURE_STORES[i].uuid,
+    event_uuid: FIXTURE_EVENTS[i].uuid,
+    event: FIXTURE_EVENTS[i]
+  }
+})
 
 export const FIXTURE_PAYMENT = FIXTURE_PAYMENTS[0]
