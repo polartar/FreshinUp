93b462c1e10d009540ca6130a32e26fab30da0d2
Babacar Cissé DIA <bcdbuddy@outlook.com>
2020-12-27 17:30:29 +0000

    fix linting

--- app/Notifications/PasswordResetNotification.php
+++ app/Notifications/PasswordResetNotification.php
@@ -36,9 +36,22 @@ class PasswordResetNotification extends ResetPassword
 
         return (new MailMessage)
             ->subject(Lang::getFromJson('Reset Password Notification'))
-            ->line(Lang::getFromJson('You are receiving this email because we received a password reset request for your account.'))
-            ->action(Lang::getFromJson('Reset Password'), url('/auth/reset-password/' . $this->token) . '?' . http_build_query(['email' => $notifiable->getEmailForPasswordReset()]))
-            ->line(Lang::getFromJson('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')]))
-            ->line(Lang::getFromJson('If you did not request a password reset, no further action is required.'));
+            ->line(Lang::getFromJson('You are receiving this email'
+                . ' because we received a password reset request for your account.'))
+            ->action(
+                Lang::getFromJson('Reset Password'),
+                url('/auth/reset-password/' . $this->token) . '?'
+                . http_build_query(['email' => $notifiable->getEmailForPasswordReset()])
+            )
+            ->line(Lang::getFromJson(
+                'This password reset link '
+                . 'will expire in :count minutes.',
+                [
+                    'count' => config('auth.passwords.'
+                    . config('auth.defaults.passwords') . '.expire')
+                ]
+            ))
+            ->line(Lang::getFromJson('If you did not request a password reset,'
+                . ' no further action is required.'));
     }
 }
