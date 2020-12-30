2c45901349f191306822c0aa537f7975d01abbfe
Babacar Cissé DIA <bcdbuddy@outlook.com>
2020-12-30 18:40:35 +0000

    update admin dashboard

--- resources/js/pages/admin/dashboard/index.vue
+++ resources/js/pages/admin/dashboard/index.vue
@@ -1,3 +1,49 @@
+<template>
+  <v-container class="px-4">
+    <v-layout row>
+      <v-flex
+        xs12
+        pa-2
+      >
+        <h2 class="f-page__title f-page__title--admin">
+          Dashboard
+        </h2>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex
+        pa-2
+        xs12
+        sm6
+      >
+        <user-requests
+          :items="users"
+        />
+      </v-flex>
+      <v-flex
+        pa-2
+        xs12
+        sm6
+      >
+        <company-requests
+          :items="companies"
+        />
+      </v-flex>
+    </v-layout>
+    <v-dialog
+      v-model="addDialog"
+      max-width="500"
+    >
+      <add-team
+        :is-platform-admin="isPlatformAdmin"
+        :companies="companiesForAddTeam"
+        @cancel="addDialog = false"
+        @submit="onAddTeamSubmit"
+      />
+    </v-dialog>
+  </v-container>
+</template>
+
 <script>
 import { mapGetters } from 'vuex'
 import { USER_TYPE } from '../../../store/modules/userTypes'
